bfc3fb3c0306c641e169cdd90177bf01710c3563
mamsama <mamsama678@gmail.com>
2021-07-02 21:22:55 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          mamsama
+          cuongthethoi
         </p>
         <a
           className="App-link"
